e1f52f809c32ab2fcfe4dfdffa6895a22c50f2e4
Vivien Ngo <hello@vivngo.xyz>
2018-01-26 13:29:33 -0500

    Make simple clock
    Assignment 1, part 2

--- sketch.js
+++ sketch.js
@@ -2,22 +2,40 @@ function setup() {
   createCanvas(640, 480);
 }
 
-function draw() {
-  background('#15534C');
+// hour()     0-23
+// minute()   0-59
 
+function draw() {
+  var change;
+
+  background('#15534C');
   noStroke();
 
+  // small circle grows according to seconds
   fill('#297159');
-  ellipse(320, 240, 380, 380);
+  change = map(second(), 0, 59, 30, 100);
+  // text(change, 300, 50);
+  ellipse(530, 100, change, change);
 
+  // largest circle grows according to hour
   fill('#73AC61');
-  ellipse(320, 240, 320, 320);
+  change = map(hour(), 0, 14, 240, 400)
+  ellipse(240, 240, change, change);
 
+  // a constant circle to frame the face
   fill('#498F60');
-  ellipse(320, 240, 240, 240);
+  ellipse(240, 240, 240, 240);
 
   fill('#E2E062');
-  textSize(32);
   textAlign(CENTER);
-  text('Hello World!', 320, 256);
+  textSize(24);
+  text('Have a good day!', 520, 380);
+
+  // face rotates according to minute
+  translate(240, 240);   // move center of rotation
+  change = map(minute(), 0, 59, -1.5*PI, 0.5*PI);
+  rotate(change);
+  textSize(128);
+  text(':-)', 0, 32);
+
 }
