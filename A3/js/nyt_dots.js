169ccfa6e504767035b71464c703285115dfbfb4
Vivien Ngo <hello@vivngo.xyz>
2018-02-11 20:19:48 -0500

    Add assignment 3: text

---
+++ A3/js/nyt_dots.js
@@ -0,0 +1,72 @@
+var headlines = [];
+var nytFont;
+
+function preload() {
+
+  // Assemble url for API call
+  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+  var apikey = "5565931934c74ea4813794dbeabd282e"; // see: https://developer.nytimes.com
+  url += "?api-key=" + apikey;
+
+  nytResponse = loadJSON(url);
+  // loadJSON() is asynchronous, but calling it inside preload() guarantees
+  // we'll have a response before setup() and draw() is run.
+}
+
+function setup() {
+  createCanvas(960, 500);
+  background(50);
+
+  console.log(nytResponse);
+
+  extractHeadlines();
+}
+
+function draw() {
+  background(50);
+  noStroke();
+
+  var margin = 30;
+  translate(margin, margin);
+
+  for (var i = 0; i < headlines.length; i++) {
+    // split into words, sorted by length, greatest to least
+    var words = headlines[i].split(" ");
+
+    // draw rectangles for each word
+    var offset = 0;
+    var max = 0, min = 100;
+
+    fill('rgba(150,150,230,0.6)');
+    for (var j = 0; j < words.length; j++) {
+      var wordWidth = textWidth(words[j]);
+      if (wordWidth > max) {
+        max = wordWidth;
+      } else if (wordWidth < min) {
+        min = wordWidth;
+      }
+
+      ellipse(i*20, map(wordWidth, 0, 100, 0, 480), 8);
+
+      // four pixels between words
+      offset += wordWidth + 5;
+    }
+    stroke('rgba(180,150,180,0.2)');
+    line(i*20, map(max, 0, 100, 0, 480), i*20, map(min, 0, 100, 0, 480));
+
+    // draw headline
+    // fill(150);
+    // text(headlines[i], 0, i*lineheight);
+  }
+}
+
+function extractHeadlines() {
+  for (var i = 0; i < nytResponse.results.length; i++) {
+    var h = nytResponse.results[i].title;
+    // besides .title, other text data available to you include:
+    // .abstract, .byline, .section, etc. etc.
+    append(headlines, h);
+  }
+
+  // console.log(headlines); // make sure counted data looks as expected
+}
---
+++ A3/js/nyt_rects.js
@@ -0,0 +1,75 @@
+var headlines = [];
+var nytFont;
+
+function preload() {
+
+  // Assemble url for API call
+  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+  var apikey = "5565931934c74ea4813794dbeabd282e"; // see: https://developer.nytimes.com
+  url += "?api-key=" + apikey;
+
+  nytResponse = loadJSON(url);
+  // loadJSON() is asynchronous, but calling it inside preload() guarantees
+  // we'll have a response before setup() and draw() is run.
+
+  // Load NYT font
+  nytFont = loadFont('assets/cheltenham-normal-300.ttf');
+}
+
+function setup() {
+  createCanvas(640, 1200);
+  background(50);
+  textFont(nytFont);
+
+  console.log(nytResponse);
+
+  extractHeadlines();
+}
+
+function draw() {
+  background(50);
+  textSize(16);
+  noStroke();
+  textAlign(LEFT, TOP);
+
+  var margin = 40;
+  translate(margin, margin);
+
+  for (var i = 0; i < headlines.length; i++) {
+    // split into words, sorted by length, greatest to least
+    var words = headlines[i].split(" ");
+    words.sort(function(a, b) {
+      return textWidth(b) - textWidth(a);
+    });
+
+    // draw rectangles for each word
+    var offset = 0;
+    for (var j = 0; j < words.length; j++) {
+      var wordWidth = textWidth(words[j]);
+      fill(map(wordWidth, 0, 110, 200, 55));  // darker if longer
+      rect(offset, i*25, wordWidth+1, 20);
+      if (mouseIsPressed) {
+        console.log("click!");
+        fill(0);
+        text(words[j], offset+0.3, i*25);
+      }
+      // four pixels between words
+      offset += wordWidth + 5;
+    }
+
+    // draw headline
+    // fill(150);
+    // text(headlines[i], 0, i*lineheight);
+  }
+}
+
+function extractHeadlines() {
+  for (var i = 0; i < nytResponse.results.length; i++) {
+    var h = nytResponse.results[i].title;
+    // besides .title, other text data available to you include:
+    // .abstract, .byline, .section, etc. etc.
+    append(headlines, h);
+  }
+
+  // console.log(headlines); // make sure counted data looks as expected
+}
