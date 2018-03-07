b14e01e39746f910434e6f94e20bce0213d66e09
Vivien <vivngo@Viviens-MacBook-Air.local>
2018-03-07 02:32:19 -0500

    Update A4
    Order by community board name, order by number gardens

--- A4/gardens.js
+++ A4/gardens.js
@@ -1,24 +1,63 @@
 var table;
 var gardenCounts = {};
+var keysABC = [];
+var keys123 = [];
 var maxGardens = 0, maxGardensCBs = [];
 
-// Display "Loading..." on the screen so we see something's happening
+// drawing constants
+var dotW = 5;
+var labelTextSize = 10;
+// var margin = 50;
+// Colors for each borough
+var colors = {  M: '#308890',     // Manhattan
+                B: '#E87182',     // Brooklyn
+                Q: '#D8AF39',     // Queens
+                X: '#58A449',     // Bronx
+                N: '#7772AF',     // N/A
+                R: '#E48C2A'};    // Staten Island?
+
+// state variables
+var state = 0; // 0:= alphabetical; 1:= by number of gardens
+
+// Display 'Loading...' on the screen so we see something's happening
 function preload() {
   table = loadTable('NYC_Greenthumb_Community_Gardens_short.csv', 'csv', 'header');
-  console.log(table);
 }
 
-// In this sketch everything happens in setup
 function setup() {
+  button = createButton('toggle');
+  button.mousePressed(toggleState);
+
   // createCanvas(windowWidth, windowHeight);
   createCanvas(1440, 620);
-  background(50);
   loadData();
-  noLoop();
+
+  textAlign(CENTER);
+  textSize(labelTextSize);
+}
+
+function draw() {
+  background('#403B38');
+  stroke(153);
+  // line(20, 20, 20, 580);    // y-axis
+  line(20, 580, 1430, 580); // x-axis
+
+  noStroke();
+  var x = 40;
+  var keys = (state == 0) ? keysABC : keys123;
+  for (var i = 0; i < keys.length; i++) {
+    for (var j = 0; j < gardenCounts[keys[i]]; j++) {
+      drawFlowerBud(x, 570-j*10, keys[i].charAt(0));
+    }
+    fill(153);
+    text(keys[i], x, 600);
+    x += 25;
+  }
+
 }
 
 function loadData() {
-  var communityBoards = table.getColumn("Community Board");
+  var communityBoards = table.getColumn('Community Board');
 
   // iterate through all community boards and
   // count how many community gardens are in each
@@ -29,39 +68,26 @@ function loadData() {
       gardenCounts[cb] = 1;
     }
   });
-  console.log(gardenCounts);
 
-  // find which community boards have the most gardens
-  for (var cb in gardenCounts) {
-    if (gardenCounts[cb] > maxGardens) {
-      maxGardens = gardenCounts[cb];
-      maxGardensCBs.push(cb);
+  // order by community board name
+  for (var k in gardenCounts) {
+    if (gardenCounts.hasOwnProperty(k)) {
+      keysABC.push(k);
+      keys123.push(k);
     }
   }
-
-  console.log("Max # gardens: " + maxGardens);
-  console.log("Found in CB's: " + maxGardensCBs);
+  keysABC.sort();
+  keys123.sort(function(a,b){return gardenCounts[b]-gardenCounts[a]});
+  console.log(gardenCounts);
 
 }
 
-function draw() {
-  // draw axes
-  stroke(153);
-  line(20, 20, 20, 580);    // y
-  line(20, 580, 1430, 580); // x
+function toggleState() {
+  state = (state == 0) ? 1 : 0;
+}
 
-  noStroke();
-  textSize(10);
-  textAlign(CENTER);
-  var x = 40;
-  for (var cb in gardenCounts) {
-    for (var i = 0; i < gardenCounts[cb]; i++) {
-      fill('rgba(255,100,230,0.6)');
-      ellipse(x, 570-i*10, 5);
-    }
-    fill(153);
-    text(cb, x, 600);
-    x += 25;
-  }
-
+function drawFlowerBud(x, y, colorKey) {
+  fill('rgba(255,100,230,0.6)');
+  fill(colors[colorKey]);
+  ellipse(x, y, dotW);
 }
